e809f245915249616c4687e7d8ad6273ed689289
lima1909 <lima1909@github.com>
2018-01-15 22:33:55 +0100

    Example ping - pong to illustrate the concurrentcy pattern (with channel) in golang.

---
+++ concurrent/README.md
@@ -0,0 +1,3 @@
+Example ping - pong to illustrate the concurrentcy pattern (with channel) in golang.
+
+A documentation can you find her: [example](http://divan.github.io/posts/go_concurrency_visualize/)
---
+++ concurrent/concurrent.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Player ping or pong
+type Player string
+
+// Ball tokick from ping an pong and count the hits
+type Ball struct {
+	hits          int
+	currentPlayer Player
+}
+
+func (ball Ball) String() string {
+	return fmt.Sprintf("[hit: %d from: %s]", ball.hits, ball.currentPlayer)
+}
+
+// Dir list file and directories recursive from start diectory
+func play(player Player, ballChan chan *Ball) {
+	for {
+		// receive value from chain
+		// other go-routine is blocked and can not receive new data
+		ball := <-ballChan
+		ball.hits++
+		ball.currentPlayer = player
+		dur := time.Millisecond * time.Duration(rand.Intn(1000))
+		time.Sleep(dur)
+		fmt.Printf("Play: %v -- [%v] \n", ball.String(), dur)
+		// ready and send result to chain
+		ballChan <- ball
+	}
+}
+
+func run() {
+	var (
+		ping Player = "ping"
+		pong Player = "pong"
+	)
+
+	ballChan := make(chan *Ball)
+
+	// start 2 player and wait to send data on channel
+	go play(ping, ballChan)
+	go play(pong, ballChan)
+
+	// init new Ball and send to Channel
+	// !!! block channel !!!
+	// receiver must start first, otherwise nobody can't unlock
+	ballChan <- new(Ball)
+	time.Sleep(time.Second * 3)
+	// release channel, the main functions is completed
+	<-ballChan
+}
+
+func main() {
+	run()
+}
+
+// To Examples to illustrate the Problem vom blocking Channel:
+
+// first: start go-function (in background) with receive value from channel
+// secend: send value to channel
+func simpleExampleWork() {
+	c := make(chan int)
+	fmt.Println("start")
+
+	go func() {
+		fmt.Println("receive: ", <-c)
+	}()
+	c <- 1
+}
+
+// deadlock - send before can't receive value from channel
+func simpleExampleDoNotWork() {
+	c := make(chan int)
+	fmt.Println("start")
+	// blocked !!!
+	c <- 1
+	// this go function was NEVER reached
+	go func() {
+		fmt.Println("receive: ", <-c)
+	}()
+}
